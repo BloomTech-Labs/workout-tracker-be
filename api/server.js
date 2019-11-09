4b6b56e647f99be4d44c42df01c5dd553fe9f8ef
cwern40 <chris.wernli@hotmail.com>
2019-11-09 12:25:38 -0700

    I created the basic file structure and created two routes, one for auth and one for routines

--- api/server.js
+++ api/server.js
@@ -5,10 +5,20 @@ require('dotenv').config();
 
 const server = express();
 
+const authRouter = require('../auth/auth-router');
+const routinesRouter = require('../routines/routines-router');
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 server.use(express.static(__dirname + '/../client/build/'));
+
+server.use('/api/auth', authRouter);
+server.use('/api/routines', routinesRouter);
+
+server.get('/', (req, res) => {
+    res.send("I guess it's working!")
+});
 
 module.exports = server;
