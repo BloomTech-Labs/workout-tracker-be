23896418252c588c9d04ace04952c006a31741dc
heuzin <matheusqs.arq@gmail.com>
2019-11-14 18:18:52 -0500

    Route for memberRecords created

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const server = express();
 
 const MembersRouter = require('../members/members-router')
 const MembersStatusRouter = require('../membersStatus/memberStatus-router')
+const MembersRecordsRouter = require('../memberRecords/memberRecords-router')
 
 server.use(helmet());
 server.use(cors());
@@ -14,6 +15,7 @@ server.use(express.json());
 
 server.use('api/members', MembersRouter);
 server.use('api/membersstatus', MembersStatusRouter);
+server.use('api/membersrecords', MembersRecordsRouter);
 server.use(express.static(__dirname + '/../client/build/'));
 
 module.exports = server;
