0932c911cf455ac8513f697410ad4c539d7e2837
Rod <rod.bromberg@yahoo.com>
2019-12-10 18:57:11 -0500

    auth changes

---
+++ auth/jwt.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const Members = require("../members/members-model");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "secret";
+// //opts.issuer = "accounts.examplesoft.com";
+// opts.audience = "yoursite.net";
+passport.use(
+  new JwtStrategy(opts, function(payload, done) {
+    Members.findBydId(payload.id)
+      .then(function(user) {
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      })
+      .catch(err => {
+        if (err) {
+          return done(err, false);
+        }
+      });
+  })
+);
