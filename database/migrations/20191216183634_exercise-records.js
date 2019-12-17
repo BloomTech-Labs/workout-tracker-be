3f4e8ef2bdcf63fbe418ffbefdf93d4f10c0c79e
cwern40 <chris.wernli@hotmail.com>
2019-12-16 18:48:52 -0700

    Creating the migration file for the exercise records table

---
+++ database/migrations/20191216183634_exercise-records.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('exercise_record', tbl => {
+        tbl.increments();
+        tbl.integer('exercise_id');
+        tbl.integer('routine_id')
+          .notNullable()
+          .unsigned()
+          .references('id')
+          .inTable('routines')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+        tbl.integer('routine_record_id')
+          .notNullable()
+          .unsigned()
+          .references('id')
+          .inTable('member_routine_record')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+        tbl.integer('sets').notNullable();
+        tbl.integer('reps').notNullable();
+        tbl.string('notes');
+        tbl.timestamp('exercise_date').defaultTo(knex.fn.now());
+    })
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('exercise_record')
+  };
+  
