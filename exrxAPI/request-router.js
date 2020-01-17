c1488725e93c69ff21fcd505e2c43a53a646b9bc
cwern40 <chris.wernli@hotmail.com>
2020-01-16 18:50:29 -0700

    added error handling in the exrx router

--- exrxAPI/request-router.js
+++ exrxAPI/request-router.js
@@ -6,42 +6,60 @@ const router = express.Router();
 let bearer
 
 router.post("/", async(req, res) => {
+    try {
+        const { search } = req.body
 
-    const { search } = req.body
+        if (search) {
+            await new Promise(function(resolve, reject) {
+                request.post('http://204.235.60.194/consumer/login', {form:{username: process.env.EXRX_USERNAME, password: process.env.EXRX_PASSWORD}}, function(err, httpResponse, body) {
+                bearer = JSON.parse(body);
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(bearer)
+                    }
+                })
+            })
+            
+            const searchRequest = req.body.search;
 
-    if (search) {
-        const tokenRequest = await request.post('http://204.235.60.194/consumer/login', {form:{username: process.env.EXRX_USERNAME, password: process.env.EXRX_PASSWORD}}, function(err, httpResponse, body) {
-            bearer = JSON.parse(body);
-        })
-
-        await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-        const test = await console.log(JSON.stringify(bearer.token));
-        
-        const searchRequest = req.body.search;
-
-        const getURL = {
-            url: Number(searchRequest) ? `http://204.235.60.194/exrxapi/v1/allinclusive/exercises?exerciseids=[${searchRequest}]` : `http://204.235.60.194/exrxapi/v1/allinclusive/exercises?exercisename=${searchRequest}`,
-            headers: {
-                "Content-type": "application/json",
-                "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13",
-                "Connection": "keep-alive",
-                "Accept": "*/*",
-                "accept-encoding": "gzip, deflate",
-                "Authorization": "Bearer " + bearer.token
+            const getURL = {
+                url: Number(searchRequest) ? `http://204.235.60.194/exrxapi/v1/allinclusive/exercises?exerciseids=[${searchRequest}]` : `http://204.235.60.194/exrxapi/v1/allinclusive/exercises?exercisename=${searchRequest}`,
+                headers: {
+                    "Content-type": "application/json",
+                    "User-Agent": process.env.EXRX_USER_AGENT,
+                    "Connection": "keep-alive",
+                    "Accept": "*/*",
+                    "accept-encoding": "gzip, deflate",
+                    "Authorization": "Bearer " + bearer.token
+                }
             }
-        }
 
-        function callback(error, response, body) {
-            console.log(error);
-            console.log(response.statusCode);
-            console.log(body);
-            res.send(body);
-        }
+            function callback(error, response, body) {
+                body = JSON.parse(body)
+            
+                if (error) {
+                    res.status(response.statusCode).json({
+                        message: error
+                    })
+                } else if (body.success == false) {
+                    res.status(401).json({
+                        message: body.message
+                    });
+                } else {
+                    res.send(body);
+                }
+            }
 
-        const getRequest = await request(getURL, callback);
-    } else {
-        res.status(400).json({
-            message: "missing a search parameter"
+            const getRequest = await request(getURL, callback);
+        } else {
+            res.status(400).json({
+                message: "missing a search parameter"
+            })
+        }
+    } catch {
+        res.status(500).json({
+            message: "Failed to get exercises"
         })
     }
 });
