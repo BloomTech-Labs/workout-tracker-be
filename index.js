64d22e451b53448c6ee51c69054dd62106d055a3
mathqs <46359247+mathqs@users.noreply.github.com>
2020-01-22 00:07:11 -0200

    env

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const server = require('./api/server.js');
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`\n=== Server listening on port ${PORT} ===\n`)
-});
+}); 
