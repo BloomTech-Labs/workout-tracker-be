549bd027b898cbd4eedf806341bc3f1e33241e54
cwern40 <chris.wernli@hotmail.com>
2020-01-29 17:41:30 -0700

    fixing the member router to match master

--- members/members-router.js
+++ members/members-router.js
@@ -20,7 +20,6 @@ const router = express.Router();
 
 router.get(
   "/",
-  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Members.find()
       .then(members => {
@@ -89,24 +88,26 @@ router.post(
       if (err) {
         return res.json({ err });
       }
-      return res.json({ token });
-    });
-    //return res.json(req.user);
-    let { username, password } = req.body;
-    Members.findBy({ username })
+      // Get member
+      let { username, password } = req.body;
+      Members.findBy({ username })
       .first()
       .then(user => {
         if (username && password) {
-          res.status(200).json({
-            message: `Welcome ${user.first_name}!`
+          return res.status(200).json({
+            message: `Welcome ${user.first_name}!`,
+            userId: user.id,
+            token
           });
         } else {
-          res.status(401).json({ message: "Invalid Credentials" });
+          return res.status(401).json({ message: "Invalid Credentials" });
         }
       })
       .catch(error => {
         res.status(500).json(error);
       });
+    });
+    //return res.json(req.user);
   }
 );
 
