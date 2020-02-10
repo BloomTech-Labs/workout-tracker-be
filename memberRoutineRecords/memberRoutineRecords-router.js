1a1fe7037c8ec4aae7ef59905820e36434727a6b
heuzin <matheusqs.arq@gmail.com>
2020-02-10 18:36:16 -0500

    cleanning up code

--- memberRoutineRecords/memberRoutineRecords-router.js
+++ memberRoutineRecords/memberRoutineRecords-router.js
@@ -4,26 +4,5 @@ const memberRoutineRecords = require('./memberRoutineRecords-model')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    memberRoutineRecords.find()
-  .then(memberRoutineRecords => {
-    res.status(200).json(memberRoutineRecords);
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to get exercise records' });
-  });
-});
-
-router.post('/', (req, res) => {
-  const memberExerciseData = req.body;
-
-  memberRoutineRecords.add(memberExerciseData)
-  .then(newMemberExercise => {
-    res.status(200).json(newMemberExercise);
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to create new exercise record' });
-  });
-});
 
 module.exports = router;
